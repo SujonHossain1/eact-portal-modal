7b4e985c2ea3d382e8586e08db1c4872c1539114
Sujon Hossain <sujon.hossain758@gmail.com>
2021-12-10 11:36:40 +0600

    update example with readme

--- example/Example.tsx
+++ example/Example.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
 import Modal from '../src/index';
 
-const modalBodyStyleDefault: React.CSSProperties = {
+const overlayStyle: React.CSSProperties = {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: ' 0',
+    bottom: '0',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: '1000',
+};
+
+const modalBodyStyle: React.CSSProperties = {
     position: 'fixed',
     top: '50%',
     left: '50%',
@@ -28,7 +38,8 @@ const Example = () => {
             <Modal
                 isOpen={isOpen}
                 onClose={onClose}
-                // modalBodyStyle={modalBodyStyleDefault}
+                modalBodyStyle={modalBodyStyle}
+                overlayStyle={overlayStyle}
             >
                 <button onClick={onClose}>Close</button>
                 <h1
